a7fe6974c60f29cfc2efbf8df93c8afc26dedc9a
zhouxin <mfjt55@163.com>
2026-09-06 18:51:18 +0800

    修 dead_strip 关闭方式: 新版 ld 报 unknown options: -no_dead_strip, 改为 CMAKE_XCODE_ATTRIBUTE_DEAD_CODE_STRIPPING NO 从工程设置源头去掉 -dead_strip(iOS 链接行的该 flag 即此 setting 生成)

--- cmake/LinkVulkan.cmake
+++ cmake/LinkVulkan.cmake
@@ -53,8 +53,9 @@ if(APPLE)
     if(Vulkan_MoltenVK_LIBRARY AND Vulkan_MoltenVK_INCLUDE_DIR)
         target_link_libraries(vulkan INTERFACE ${Vulkan_MoltenVK_LIBRARY})
         target_include_directories(vulkan SYSTEM INTERFACE ${Vulkan_MoltenVK_INCLUDE_DIR})
-        # Xcode 15 ld 对 -dead_strip + MoltenVK prelink 静态库组合会静默崩溃(iOS 插件 dylib 复现), 关闭 dead_strip
-        target_link_options(vulkan INTERFACE "-Wl,-no_dead_strip")
+        # Xcode 15 ld 对 -dead_strip + MoltenVK prelink 静态库组合会静默崩溃(iOS 插件 dylib 复现);
+        # 从工程设置关 dead_strip(-Wl,-no_dead_strip 拼写新版 ld 不认)
+        set(CMAKE_XCODE_ATTRIBUTE_DEAD_CODE_STRIPPING NO)
         # 复制 MoltenVK 库到构建目录
         # /Users/zhouxin/VulkanSDK/1.4.313.0/iOS/lib/MoltenVK.xcframework
         file(COPY ${Vulkan_MoltenVK_LIBRARY} DESTINATION "${CMAKE_INSTALL_PREFIX}")
