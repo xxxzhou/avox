66ca31b4c0af0a514c0795cbe36cabbaa1d2daca
zhouxin <mfjt55@163.com>
2026-09-06 18:36:38 +0800

    Apple 链接修 dead_strip 崩溃: Xcode 15 ld_prime 对 -dead_strip + MoltenVK prelink 静态库组合静默崩溃(iOS avox_fbx dylib 200ms 无诊断失败, mac 链接行无 dead_strip 故成功), vulkan 接口库加 -Wl,-no_dead_strip

--- cmake/LinkVulkan.cmake
+++ cmake/LinkVulkan.cmake
@@ -53,6 +53,8 @@ if(APPLE)
     if(Vulkan_MoltenVK_LIBRARY AND Vulkan_MoltenVK_INCLUDE_DIR)
         target_link_libraries(vulkan INTERFACE ${Vulkan_MoltenVK_LIBRARY})
         target_include_directories(vulkan SYSTEM INTERFACE ${Vulkan_MoltenVK_INCLUDE_DIR})
+        # Xcode 15 ld 对 -dead_strip + MoltenVK prelink 静态库组合会静默崩溃(iOS 插件 dylib 复现), 关闭 dead_strip
+        target_link_options(vulkan INTERFACE "-Wl,-no_dead_strip")
         # 复制 MoltenVK 库到构建目录
         # /Users/zhouxin/VulkanSDK/1.4.313.0/iOS/lib/MoltenVK.xcframework
         file(COPY ${Vulkan_MoltenVK_LIBRARY} DESTINATION "${CMAKE_INSTALL_PREFIX}")
